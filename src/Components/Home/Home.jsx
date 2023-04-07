5d4de96fff54ad48961a65456924069ee6ae25f1
mdfaysal22 <mdfaysal.freelancer@gmail.com>
2023-04-08 00:45:28 +0600

    Sound System apply

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,17 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Aside from "./Aside/Aside";
 import Cards from "./Cards/Cards";
 import { toast } from "react-hot-toast";
-
+import notification from './../../../public/noti.mp3';
 const Home = () => {
   const [bookedData, setBookedData] = useState([]);
   const [totalTime, setTotalTime] = useState(0);
+  const audioPlayer = useRef(null);
 
+  const notificationhandler = () => {
+    audioPlayer.current.play();
+  }
   const readTime = (number) => {
     let time = totalTime + number;
     setTotalTime(time);
   };
-console.log(bookedData);
   const handleBookmark = (title) => {
     let bookmarkedItems = [];
 
@@ -24,6 +27,7 @@ console.log(bookedData);
     if (!alreadyBooked) {
       bookmarkedItems.push(title);
       setBookedData(bookmarkedItems);
+      notificationhandler();
       toast.success("Bookmarked Successful");
     } else {
       toast.error("Already Bookmarked");
@@ -40,6 +44,7 @@ console.log(bookedData);
     <div className="grid gap-4 mt-4 grid-cols-3">
       <div className="col-span-2">
         <Cards readTime={readTime} handleBookmark={handleBookmark}></Cards>
+        <audio ref={audioPlayer} src={notification}></audio>
       </div>
       <div className="col-span-1">
         <Aside bookedData={bookedData} totalTime={totalTime}></Aside>
